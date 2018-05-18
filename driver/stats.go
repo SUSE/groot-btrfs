9d9e0587c1fce82d70f8c649cb16cfa379de1634
Dimitris Karakasilis <DKarakasilis@suse.com>
2018-05-18 15:03:27 +0300

    Implement Stats()

--- driver/stats.go
+++ driver/stats.go
@@ -1,10 +1,51 @@
 package driver
 
 import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strings"
+
 	"code.cloudfoundry.org/groot"
+	"code.cloudfoundry.org/grootfs/store"
 	"code.cloudfoundry.org/lager"
+	errorspkg "github.com/pkg/errors"
 )
 
 func (d *Driver) Stats(logger lager.Logger, bundleID string) (groot.VolumeStats, error) {
-	return groot.VolumeStats{}, nil
+	imagePath := filepath.Join(d.conf.StorePath, store.ImageDirName, bundleID)
+
+	return d.FetchStats(logger, imagePath)
+}
+
+func (d *Driver) FetchStats(logger lager.Logger, imagePath string) (groot.VolumeStats, error) {
+	logger = logger.Session("btrfs-fetching-stats", lager.Data{"imagePath": imagePath})
+	logger.Debug("starting")
+	defer logger.Debug("ending")
+
+	args := []string{
+		"--btrfs-bin", d.conf.BtrfsBinPath(),
+		"stats",
+		"--volume-path", filepath.Join(imagePath, "rootfs"),
+		"--force-sync",
+	}
+
+	stdoutBuffer, err := d.runDrax(logger, args...)
+	if err != nil {
+		return groot.VolumeStats{}, err
+	}
+
+	usageRegexp := regexp.MustCompile(`.*\s+(\d+)\s+(\d+)$`)
+	usage := usageRegexp.FindStringSubmatch(strings.TrimSpace(stdoutBuffer.String()))
+
+	var stats groot.VolumeStats
+	if len(usage) != 3 {
+		logger.Error("parsing-stats-failed", errorspkg.Errorf("raw stats: %s", stdoutBuffer.String()))
+		return stats, errorspkg.New("could not parse stats")
+	}
+
+	fmt.Sscanf(usage[1], "%d", &stats.DiskUsage.TotalBytesUsed)
+	fmt.Sscanf(usage[2], "%d", &stats.DiskUsage.ExclusiveBytesUsed)
+
+	return stats, nil
 }
