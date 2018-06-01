b8e93871deb55d60d6caeeff4a65b5bfc8c83eb5
Dimitris Karakasilis <DKarakasilis@suse.com>
2018-06-01 14:17:08 +0300

    Fix drax tests by creating a btrfs filesystem

--- vendor/code.cloudfoundry.org/groot/create.go
+++ vendor/code.cloudfoundry.org/groot/create.go
@@ -41,18 +41,7 @@ func (g *Groot) Create(handle string, diskLimit int64, excludeImageFromQuota boo
 		return runspec.Spec{}, errors.Wrap(err, "creating bundle")
 	}
 
-	if len(image.Config.Config.Env) > 0 {
-		if bundle.Process == nil {
-			bundle.Process = &runspec.Process{}
-		}
-
-		bundle.Process.Env = append(bundle.Process.Env, image.Config.Config.Env...)
-	}
-
-	metadata := ImageMetadata{
-		Size: image.Size,
-	}
-
+	metadata := ImageMetadata{Size: image.Size}
 	err = g.Driver.WriteMetadata(g.Logger.Session("write-metadata"), handle, metadata)
 
 	return bundle, err
